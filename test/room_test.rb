f2bc1197808e4baf9af8e996351b4553d9e579e8
Kate Tester <katemorris@gmail.com>
2020-08-26 09:07:36 -0600

    Adds comments with future test flow

--- test/room_test.rb
+++ test/room_test.rb
@@ -22,3 +22,24 @@ class RoomTest < Minitest::Test
     assert_equal 180, room2.area
   end
 end
+
+
+# pry(main)> require './lib/room'
+# #=> true
+#
+# pry(main)> room = Room.new(:bedroom, 10, "13")
+# #=> #<Room:0x00007fa53b9ca0a8 @category=:bedroom, @length=10, @width="13">
+#
+# pry(main)> room.category
+# #=> :bedroom
+#
+# pry(main)> room.area
+# #=> 130
+#
+# pry(main)> room.is_painted?
+# #=> false
+#
+# pry(main)> room.paint
+#
+# pry(main)> room.is_painted?
+# #=> true

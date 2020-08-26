8eeb9a17c5de62af68844b00e05047a52cd8d216
Kate Tester <katemorris@gmail.com>
2020-08-26 10:23:21 -0600

    Adds three skipped tests for iteration 4.

--- test/house_test.rb
+++ test/house_test.rb
@@ -78,4 +78,49 @@ class RoomTest < Minitest::Test
     assert_equal house_details_static, house.details
   end
 
+  def test_house_has_price_per_sq_ft
+    skip
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal 210.53, house.price_per_square_foot
+  end
+
+  def test_sort_rooms_by_area
+    skip
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal [room_4, room_3, room_2, room_1], house.rooms_sorted_by_area
+  end
+
+  def test_return_rooms_by_category
+    skip
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    house_rooms = {:bedroom=>[room_1, room_2], :living_room=> [room_3], :basement=> [room_4]}
+    assert_equal house_rooms, house.rooms_by_category
+  end
 end
